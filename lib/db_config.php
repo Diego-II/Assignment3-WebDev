5def86667d24cb4937f3cbfd4db177a6feda1b04
Diego Irarrazaval Infante <dirarraz@anakena.dcc.uchile.cl>
2020-06-21 10:58:44 -0400

    Db_config set to deployment

--- lib/db_config.php
+++ lib/db_config.php
@@ -1,8 +1,8 @@
 <?php
 class DbConfig{
 	private static $db_name = "cc500217_db"; //Base de datos de la app
-	private static $db_user = "usuario"; //Usuario MySQL
-	private static $db_pass = "cc5002"; //Password
+	private static $db_user = "cc500217_u" //Usuario MySQL
+	private static $db_pass = "entesquela"; //Password
 	private static $db_host = "localhost";//Servidor donde esta alojado, puede ser 'localhost' o una IP (externa o interna).
 	
 	public static function getConnection(){
